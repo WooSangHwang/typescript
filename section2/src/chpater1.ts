4a88cad0601a8fc551612312035d945dd8696cf3
황우상 <hwang-usang@hwang-usang-ui-MacBookPro.local>
2025-06-25 10:15:30 +0900

    원시 리터럴 타입

---
+++ section2/src/chpater1.ts
@@ -0,0 +1,21 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+
+//string
+let str1: string = "hello";
+let str2: string = `hello`;
+
+//boolean
+let bool1: boolean = true;
+
+//null
+let null1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+//리터럴 타입
+let numA: 10 = 10;
+let strA: "hello" = "hello";
+let boolA: true = true;
