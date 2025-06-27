12a9b239f87c02e0f0e66af8a92265ab4978a2f7
황우상 <hwang-usang@hwang-usang-ui-MacBookPro.local>
2025-06-27 10:33:01 +0900

    Omit

--- section10/src/chapter2.ts
+++ section10/src/chapter2.ts
@@ -19,3 +19,35 @@ const legacyPost: Pick<Post, "title" | "content"> = {
 type Pick2<T, K extends keyof T> = {
   [key in K]: T[key];
 };
+
+/**
+ * Omit<T, K>
+ *   생략하다,, 빼다
+ *   객체 타입으로부터 특정 프로퍼트를 제거하는 타입
+ */
+interface Post {
+  title: string;
+  tags: string[];
+  content: string;
+  thumbnailURL?: string;
+}
+//Pick
+const noTitlePost: Pick<Post, "content" | "tags" | "thumbnailURL"> = {
+  content: "",
+  tags: [],
+  thumbnailURL: "",
+};
+
+//Omit
+const noTitlePost2: Omit<Post, "title"> = {
+  content: "",
+  tags: [],
+  thumbnailURL: "",
+};
+
+//직접 구현하기
+type Omit2<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>;
+// T = Post, K = 'title'
+// Pick<Post, Exclude<keyof Post, 'title'>>
+// Pick<Post, Exclude<'title' | 'content' | 'tags' | 'thumbnailURL', 'title'>>
+// Pick<Post, 'content' | 'tags' | 'thumbnail' >
