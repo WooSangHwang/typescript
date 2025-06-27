b2275f4daf4d97229d8606e376293ec610e25dca
황우상 <hwang-usang@hwang-usang-ui-MacBookPro.local>
2025-06-27 11:05:00 +0900

    returnType

--- section10/src/chapter3.ts
+++ section10/src/chapter3.ts
@@ -13,3 +13,25 @@ type Exclude2<T, U> = T extends U ? never : T;
 type B = Extract<string | boolean, boolean>;
 //직접구현
 type Extract2<T, U> = T extends U ? T : never;
+
+/**
+ * ReturnType<T>
+ * 함수의 반환값 타입을 추출하는 타입
+ */
+
+function funcA() {
+  return "hello";
+}
+function funcB() {
+  return 10;
+}
+
+type ReturnA = ReturnType<typeof funcA>;
+type ReturnB = ReturnType<typeof funcB>;
+
+//직접 구현하기
+type ReturnType2<T extends (...args: any) => any> = T extends (
+  ...agrs: any
+) => infer R
+  ? R
+  : never;
