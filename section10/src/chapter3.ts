e1b4183f07e8b15aa90748bd02530eb3e880df04
황우상 <hwang-usang@hwang-usang-ui-MacBookPro.local>
2025-06-27 10:54:40 +0900

    Exclude

--- section10/src/chapter3.ts
+++ section10/src/chapter3.ts
@@ -5,3 +5,11 @@
 type A = Exclude<string | boolean, boolean>;
 //직접구현
 type Exclude2<T, U> = T extends U ? never : T;
+
+/**
+ * Exclude<T, U>
+ *   T에서 U를 추출하는 타입
+ */
+type B = Extract<string | boolean, boolean>;
+//직접구현
+type Extract2<T, U> = T extends U ? T : never;
